35e12ddd1d31b4cfa8d6cff67422e8b2ca27a303
Ali Alboainin <44374885+AliAlboainin96@users.noreply.github.com>
2020-08-19 21:47:17 +0300

    Fixing some issues

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,38 @@ fn main() {
         _ => unreachable!("No language have been selected!"),
     };
 
-    match matches.value_of("License").unwrap() { 
-    "
+    match matches.value_of("licen").unwrap() {
+        "GPLv3" =>
+        {
+            println!("Adding License=GPLv3");
+        },
+
+        "AGPLv3" =>
+        {
+            println!("Adding License=AGPLv3");
+        },
+
+        "LGPLv3" =>
+        {
+            println!("Adding License=LGPLv3");
+        },
+
+        "MOZILLA-PUBLIC-LICENSE-2" =>
+        {
+            println!("Adding License=MOZILLA-PUBLIC-LICENSE-2");
+        },
+
+        "MIT" =>
+        {
+            println!("Adding License=MIT");
+        },
+
+        "APACHE-2" =>
+        {
+            println!("Adding License=APACHE-2");
+        },
+ 
+        _ => unreachable!("No License have been selected!"),
+
     };
 }
