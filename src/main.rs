5886ff8fb32a7432e7cc9172a8853fe48949c4bb
Ali Alboainin <44374885+AliAlboainin96@users.noreply.github.com>
2020-08-19 21:08:13 +0300

    Added CLI.yamr

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
+#![allow(unused_parens)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
 extern crate clap;
 extern crate regex;
-use clap::App;
-use regex::Regex;
+
+use clap::{App, load_yaml};
 
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.value_of("lang").unwrap() {
+        "none" => println!("No Language have been selected!"),
+        
+        "cpp" => 
+        {
+            println!("Hello");
+        },
+        
+        "c" => 
+        {
+            println!("مرحبا");
+        },
+
+        "rust" =>
+        {
+            println!("Hallo");
+        },
+
+        "python" => 
+        {
+            println!("Pythos goes brrr....");
+        },
+        
+        _ => unreachable!("see possible_values in yaml, handled by clap"),
+    };
 }
